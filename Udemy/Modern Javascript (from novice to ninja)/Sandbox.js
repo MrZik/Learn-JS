fbaf934a99038fad6eca17b05855646f33539afc
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-21 20:40:59 +0800

    Update Sandbox.js

---
+++ Udemy/Modern Javascript (from novice to ninja)/Sandbox.js
@@ -0,0 +1,25 @@
+// strings
+// let a = 'Ezekiel';
+// console.log('Hello');
+// console.log('Hello' + ' ' + 'World');
+// console.log(a[3]);
+// console.log(a.length);
+// let e = a.toUpperCase();
+// console.log(a);
+// console.log(e.includes("E"));
+// from what index and how many total
+//let result = a.slice(0,5);
+//let result = a.substring(2,7);
+// let result = a.replace('E','i');
+// console.log(result);
+
+// numbers
+// B I D M A S
+let radius = 10;
+const PI = 3.14;
+console.log(radius**2 * PI);
+console.log(5 * (10-3)**2);
+// use then increment
+console.log(radius++);
+// increment then use, returns 12 in this case because we use radius++ above
+console.log(++radius);
