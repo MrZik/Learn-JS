738633a778c46ec8ee4c1f823beb7a1b3f77d5af
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-21 18:15:47 +0800

    Create ArrayPrototypeLast.js

---
+++ Leetcode/ArrayPrototypeLast.js
@@ -0,0 +1,17 @@
+// 76 ms
+Array.prototype.last = function() {
+    if(this.length === 0) {
+        return -1;
+    } else {
+        return this[this.length - 1];
+    }
+};
+
+Array.prototype.last = function() {
+    return this.length === 0 ? -1 : this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
