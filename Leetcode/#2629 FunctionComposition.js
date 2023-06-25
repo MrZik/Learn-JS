da15c0f9dd606b97ed2dbc05bfb110bbd12c59a9
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-25 21:07:55 +0800

    Create #2629 FunctionComposition.js

---
+++ Leetcode/#2629 FunctionComposition.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    if(functions.length === 0){
+        return function(x) {
+            return x;
+        };
+    };
+	
+    return function(x){
+        let result = x;
+        for(let i = functions.length - 1; i >= 0; --i){
+            result = functions[i](result);
+        };
+        return result;
+    };
+};
+
+var compose = function(functions) {
+    return x => functions.reduceRight((acc,f)=>f(acc),x);
+};
+
+var compose = (functions) => functions.reduceRight((a, b) => (c) => b(a(c)), c => c)
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
