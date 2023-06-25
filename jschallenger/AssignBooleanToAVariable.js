ecd692b651b2e17a96ba11954d61dc815dab9df3
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-25 22:30:33 +0800

    Create AssignBooleanToAVariable.js

---
+++ jschallenger/AssignBooleanToAVariable.js
@@ -0,0 +1,15 @@
+// this will be undefined as well not like C#!!!!!
+let isTrue;
+console.log(isTrue);
+
+// All tge following values are falsy
+
+// false
+// null
+// undefined
+// 0
+// -0
+// NaN
+// An empty string ('')
+
+// All numbers except 0 are truthy
