3fbf7fd972c449ada9e766019f1c026fed8232e6
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-26 12:43:22 +0800

    Create BasicJavascriptMathOperators.js

---
+++ jschallenger/BasicJavascriptMathOperators.js
@@ -0,0 +1,7 @@
+function MyFunction(a,b,c,d,e,f){
+    return ((((a + b) - c) * d) / e)**f;
+}
+
+console.log(MyFunction(6,5,4,3,2,1));
+console.log(MyFunction(6,2,1,4,2,3));
+console.log(MyFunction(2,3,6,4,2,3));
