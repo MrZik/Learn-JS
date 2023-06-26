4104e73deeb80c1bb5becd430e3741966f9d7fb3
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-26 13:23:31 +0800

    Update CheckIfANumberIsAWholeNumber.js

--- jschallenger/CheckIfANumberIsAWholeNumber.js
+++ jschallenger/CheckIfANumberIsAWholeNumber.js
@@ -1,6 +1,11 @@
 function MyFunction(a){
     // return true for whole numers, false for floats
-    return Number.isInteger(a);
+    //return Number.isInteger(a);
+
+    // this will return a negative value if it can be rounded of
+    // or the decimal point if it cannot be rounded off
+    // then 0 if it is a whole number
+    return a - Math.floor(a) === 0;
 }
 
 console.log(MyFunction(4));
