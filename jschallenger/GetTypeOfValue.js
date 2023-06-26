4332827275b52460c2d18dc41c13d3aa55989765
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-26 12:10:05 +0800

    Create GetTypeOfValue.js

---
+++ jschallenger/GetTypeOfValue.js
@@ -0,0 +1,10 @@
+function myFunction(a){
+    return typeof a;
+}
+
+console.log(myFunction(1));
+console.log(myFunction(false));
+console.log(myFunction({}));
+console.log(myFunction(null));
+console.log(myFunction('string'));
+console.log(myFunction(['array']));
