86bcd2799221b360e8e84fa1f3224280b709acb8
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-26 12:08:26 +0800

    Create ComparisonOperatorsStrictEquality.js

---
+++ jschallenger/ComparisonOperatorsStrictEquality.js
@@ -0,0 +1,8 @@
+function myFunction(a, b){
+    return a === b;
+}
+
+console.log(myFunction(1,2));
+console.log(myFunction(2,3));
+console.log(myFunction(1,'1'));
+console.log(myFunction('10','10'));
