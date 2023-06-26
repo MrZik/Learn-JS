8673bcb6f9668fc4e19bab00dd472fe9a38840b7
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-26 13:14:58 +0800

    Create HowManyTimesDoesACharacterOccur.js

---
+++ jschallenger/HowManyTimesDoesACharacterOccur.js
@@ -0,0 +1,9 @@
+function MyFunction(a, b){
+    //return b.split(a).length - 1;
+    //return b.length - b.replaceAll(a,'').length;
+}
+
+console.log(MyFunction('m', 'how many times does the character occur in this sentence?'));
+console.log(MyFunction('h', 'how many times does the character occur in this sentence?'));
+console.log(MyFunction('?', 'how many times does the character occur in this sentence?'));
+console.log(MyFunction('z', 'how many times does the character occur in this sentence?'));
