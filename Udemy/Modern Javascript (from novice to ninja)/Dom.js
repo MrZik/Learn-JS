7590559864c23f0af01a7a87ad66e739d4008078
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-27 15:49:24 +0800

    Update Dom.js

--- Udemy/Modern Javascript (from novice to ninja)/Dom.js
+++ Udemy/Modern Javascript (from novice to ninja)/Dom.js
@@ -1,10 +1,27 @@
 // store a reference to a variable
 // the document.querySelector goes to the index.html
 // starts from the top then gets the first <p> tag it finds and returns it
-const para = document.querySelector('p');
+//const para = document.querySelector('p');
 
+// . means class then the class name error
+//const classError = document.querySelector('.error');
 
-const p = document.querySelector('.error');
+// add div means div with a class name error
+//const classError = document.querySelector('body > div.error');
 
-console.log(para);
-//console.log(p);
+//console.log(para);
+//console.log(classError);
+
+// go into the document then store all the p tag as a collection
+// nodelist
+//const paras = document.querySelectorAll('p');
+// const error = document.querySelectorAll('.error');
+
+// you can still use brackets index
+//console.log(paras[2]);
+
+// paras.forEach(para => {
+//     console.log(para);
+// });
+
+// console.log(error);
