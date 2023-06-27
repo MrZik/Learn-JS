52988787c5936561330e9c6cfa7031f469a19b18
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-27 15:42:10 +0800

    Added tags in the html body

---
+++ Udemy/Modern Javascript (from novice to ninja)/Dom.js
@@ -0,0 +1,10 @@
+// store a reference to a variable
+// the document.querySelector goes to the index.html
+// starts from the top then gets the first <p> tag it finds and returns it
+const para = document.querySelector('p');
+
+
+const p = document.querySelector('.error');
+
+console.log(para);
+//console.log(p);
