ef044627f4a0c58d34c30f203f03a2fa38b06580
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-29 13:37:07 +0800

    Finished Pop-up section

--- Udemy/Modern Javascript (from novice to ninja)/Dom.js
+++ Udemy/Modern Javascript (from novice to ninja)/Dom.js
@@ -270,3 +270,23 @@
 
 // ***************************************************************************************
 // ***************************************************************************************
+
+// POP UP
+
+// const button = document.querySelector('button');
+// const popup = document.querySelector('.popup-wrapper');
+// button.addEventListener('click', () => {
+//     popup.style.display = 'block';
+// });
+
+// const closeBtn = document.querySelector('.popup-close');
+// closeBtn.addEventListener('click', () => {
+//     popup.style.display = 'none';
+// });
+
+// popup.addEventListener('click', () => {
+//     popup.style.display = 'none';
+// });
+
+// ***************************************************************************************
+// ***************************************************************************************
