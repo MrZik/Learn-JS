436b4b6f397a924505ab00921c877ab1c486774b
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-01 00:57:39 +0800

    Update forecast.js

--- Udemy/Modern Javascript (from novice to ninja)/Weather_App/scripts/forecast.js
+++ Udemy/Modern Javascript (from novice to ninja)/Weather_App/scripts/forecast.js
@@ -1,1 +1,27 @@
 const key = '	nckfV07N60CqGDEYScwohRp8E1G6lnB7';
+
+// Get city information
+const getCity = async(city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    // always put ? before for query
+    // & indicates the next query
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base+query);
+    if(response.status != 200){
+        throw new Error('cannot fetch data');
+    }
+    const data = await response.json();
+    return data[0];
+};
+
+// Get weather information
+const getWeather = async(id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base+query);
+    if(response.status != 200){
+        throw new Error('cannot fetch data');
+    }
+    const data = await response.json();
+    return data;
+};
