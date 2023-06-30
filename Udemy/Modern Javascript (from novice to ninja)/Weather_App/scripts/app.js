073cdd97311f3f08d5d2ac6a9a69e48277a19bd6
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-01 02:03:45 +0800

    FInished weather app

--- Udemy/Modern Javascript (from novice to ninja)/Weather_App/scripts/app.js
+++ Udemy/Modern Javascript (from novice to ninja)/Weather_App/scripts/app.js
@@ -19,6 +19,14 @@ const updateUI = data => {
         </div>
     `;
 
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+    
+
     // remove the d-none class if present
     if(card.classList.contains('d-none')){
         card.classList.remove('d-none');
