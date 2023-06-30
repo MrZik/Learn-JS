82e3e938b46c97705ef23c573548d9b6e4a57f93
Ezekiel <ezekieldsanchez@gmail.com>
2023-06-30 16:32:40 +0800

    Update Sandbox.js

--- Udemy/Modern Javascript (from novice to ninja)/Sandbox.js
+++ Udemy/Modern Javascript (from novice to ninja)/Sandbox.js
@@ -890,3 +890,18 @@
 // always remember when a goes first, it will ascending order
 // scores.sort((a,b) => a - b);
 // console.log(scores);
+
+// chaining array methods
+const products = [
+    {name: 'gold star', price: 30},
+    {name: 'green shell', price: 10},
+    {name: 'red star', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50},
+];
+
+// this returns a new array with the string template as it's value
+const promos = products.filter(product => product.price > 20)
+.map(product => `the ${product.name} is ${product.price / 2}`);
+
+console.log(promos);
