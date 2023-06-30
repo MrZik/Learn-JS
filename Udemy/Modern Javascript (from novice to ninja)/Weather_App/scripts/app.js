228a4bc5aae331cbad38f1c059dc42593fb444e7
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-01 01:46:49 +0800

    moved icons to icons folder

--- Udemy/Modern Javascript (from novice to ninja)/Weather_App/scripts/app.js
+++ Udemy/Modern Javascript (from novice to ninja)/Weather_App/scripts/app.js
@@ -2,11 +2,13 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = data => {
     // destructore properties
     const {cityDetails, weather} = data;
-    
+
     // update details template
     details.innerHTML = `
         <h5 class="my-3">${cityDetails.EnglishName}</h5>
