a82c0eb3459027a0ccaadf22844475bfd8830f0d
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-11 09:44:36 +0800

    Create RoundANumber.js

---
+++ jschallenger/RoundANumber.js
@@ -0,0 +1,25 @@
+// TITLE
+// Round a number to 2 decimal places
+
+// DESCRIPTION
+// Write a function that takes a number (a) as argument.
+// Round a to the 2nd digit after the decimal point.
+// Return the rounded number
+
+function myFunction(a){
+    let b = parseFloat((a * 100).toFixed(11));
+    // We don't need the toFixed here because we made sure to have 2 decimal pts using the value 100
+    //return (Math.round(b) / 100).toFixed(2);
+    return Math.round(b) / 100;
+}
+
+console.log(myFunction(2.12397));
+console.log(myFunction(3.136));
+console.log(myFunction(1.12397));
+console.log(myFunction(26.1379));
+
+// TEST CASES:                      Expected
+// myFunction(2.12397)               2.12
+// myFunction(3.136)                 3.14
+// myFunction(1.12397)               1.12
+// myFunction(26.1379)               26.14
