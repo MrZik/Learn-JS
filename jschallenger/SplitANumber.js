3a332b7246b1c260e6e226681086507ef525dca0
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-11 10:34:50 +0800

    Update SplitANumber.js

--- jschallenger/SplitANumber.js
+++ jschallenger/SplitANumber.js
@@ -7,7 +7,17 @@
 // Tipp: you might want to change the type of the number for the splitting
 
 function myFunction(a) {
-    return stringArray = a.toString().split('').map(x => Number(x));
+    //return stringArray = a.toString().split('').map(x => Number(x));
+    //return [...a+''].map(x =>+x);
+
+    let num = a;
+    let output = [];
+    while(num){
+        output.unshift(num % 10);
+        num = Math.floor(num/10);
+    }
+
+    return output;
 }
 
 console.log(myFunction(10));                //Expected [1,0]
