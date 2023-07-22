d6fabb125d020dbad20455a5a7686e5a882ec21c
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-22 18:11:19 +0800

    Updated css to fit mobile portrait and desktop view screen

--- Ability_Tester/abilityTester/constants.js
+++
@@ -1,4 +0,0 @@
-const pi = 3.14;
-const piSquared = 3.14 * 2;
-
-export { pi, piSquared };
--- Ability_Tester/abilityTester/main.js
+++ Ability_Tester/abilityTester/main.js
@@ -11,24 +11,6 @@ addHomeDetails();
 initializeLinks();
 addFooterContents();
 
-// document.querySelector("header").className = headerhome.app__header;
-
-// import { pi, piSquared } from "./constants";
-// import { setupCounter } from "./counter";
-
-// document.querySelector("#app").innerHTML = `${cubesred} , ${dictionaryred}`;
-// document.querySelector("#typingitem").addEventListener("mouseover", (e) => {
-//   document.getElementById("typingitem").innerHTML = `
-//     <img src="./assets/imgs/Highlighted/gamer-red.svg" alt="img" />
-//     <div class="item_description">
-//       <h1>Typing</h1>
-//       <p>How many words per minute can you write?</p>
-//     </div>
-//   `;
-// });
-
-// setupCounter(document.querySelector("#counter"));
-
 // **********************************************************************************
 // **********************************************************************************
 
