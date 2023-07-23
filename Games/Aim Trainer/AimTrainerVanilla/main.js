689472d38c316d52b0952c92818290e4d34e5801
Ezekiel <ezekieldsanchez@gmail.com>
2023-07-24 01:33:24 +0800

    finished result logic and try again logic

--- Games/Aim Trainer/AimTrainerVanilla/main.js
+++ Games/Aim Trainer/AimTrainerVanilla/main.js
@@ -1,50 +1,102 @@
-// 600 is the app width, 100 is the target width
-const maxwidth = 500;
 const maxheight = 400;
+const aimcount = 30;
 const app = document.getElementById("app");
 
-const rndxpos = () => Math.floor(Math.random() * maxwidth);
-const rndypos = () => Math.floor(Math.random() * maxheight);
+let currentcount = aimcount;
+let firstclick = true;
+let starttime;
+let clickcount;
+let target;
+let count;
 
-app.innerHTML = `
-    <div id="header">
-        <p id="headertitle">Remaining <span id="count">30</span></p>
-    </div>
-    <div id="container">
-        <div id="target">
-            <img id="targeticon"src="target.svg" alt="img"/>
+const rndxpos = (() => {
+  // 600 is the app width, 100 is the target width
+  const maxwidth = 500;
+  return function () {
+    return Math.floor(Math.random() * maxwidth);
+  };
+})();
+
+const rndypos = (() => {
+  // 600 is the app width, 100 is the target width, header takes 50
+  const maxheight = 400;
+  return function () {
+    return Math.floor(Math.random() * maxheight);
+  };
+})();
+
+const tryagainlogic = function () {
+  showgame();
+  initializetarget();
+};
+
+const showresult = function (time) {
+  app.innerHTML = `
+    <img id="resultimg" src="target.svg" alt="img"/>
+    <p id="resulttitle">Average time per target</p>
+    <p id="resulttime">${time}ms</p>
+    <button type="button" id="tryagainbtn">
+        <p id="btntext">Try again</p>
+    </button>
+    `;
+
+  const tryagainbtn = document.getElementById("tryagainbtn");
+  tryagainbtn.addEventListener("click", tryagainlogic);
+};
+
+const showgame = function () {
+  currentcount = aimcount;
+  firstclick = true;
+  app.innerHTML = `
+        <div id="header">
+            <p id="headertitle">Remaining <span id="count">5</span></p>
         </div>
-    </div>
-`;
+        <div id="container">
+            <div id="target">
+                <img id="targeticon"src="target.svg" alt="img"/>
+            </div>
+        </div>
+    `;
 
-const count = document.getElementById("count");
+  target = document.getElementById("target");
+  count = document.getElementById("count");
+};
+
+showgame();
 
 const updatecount = (function () {
-  let currentcount = 30;
   return function () {
     currentcount -= 1;
     return currentcount;
   };
 })();
 
-const target = document.getElementById("target");
+const targetclickevent = function () {
+  if (firstclick) {
+    starttime = new Date().getTime();
+    firstclick = false;
+  }
 
-target.style.top = "200px";
-target.style.left = "250px";
-
-target.addEventListener("click", (e) => {
-  e.stopPropagation();
-  e.preventDefault();
-
-  let clickcount = updatecount();
+  clickcount = updatecount();
+  count.innerText = `${clickcount}`;
 
   if (clickcount <= 0) {
     // finished
-
+    const endtime = new Date().getTime();
+    const avgtime = Math.floor((endtime - starttime) / aimcount);
+    showresult(avgtime);
     return;
   }
 
-  count.innerText = `${clickcount}`;
   target.style.top = `${rndypos()}px`;
   target.style.left = `${rndxpos()}px`;
-});
+};
+
+const initializetarget = function () {
+  target.style.top = "200px";
+  target.style.left = "250px";
+
+  target.addEventListener("click", targetclickevent);
+};
+
+initializetarget();
